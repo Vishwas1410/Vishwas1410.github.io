9c2eeeb7bfeb5e91a16924b5a34f1999c4d2fa46
Vishwas1410 <210305105575@paruluniversity.ac.in>
2024-05-24 12:07:19 +0530

    added favicon and edited scrollbar

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,26 @@ import RecentProjects from "./components/RecentProjects";
 import { navItems } from "@/data";
 export default function Home() {
   return (
+    
+   <html>
+    <head>
+      
+
+
+    <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png"/>
+<link rel="manifest" href="./site.webmanifest"/>
+
+
+
+
+
+
+
+
+    </head>
+    <body>
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5  ">
       <div className="max-w-7xl w-full">
         {/* <FloatingNav navItems={navItems}/> */}
@@ -16,5 +36,7 @@ export default function Home() {
        <RecentProjects></RecentProjects>
       </div>
     </main>
+    </body>
+   </html>
   );
 }
